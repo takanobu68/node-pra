6c278124c6102cc722499103e38e8a84635745aa
Takanobu <61861946+takanobu68@users.noreply.github.com>
2024-06-02 14:00:19 +0900

    ejsテンプレートエンジンの使用

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const http = require('http');
 const fs = require('fs');
+const ejs = require('ejs');
+
+const index_page = fs.readFileSync('./index.ejs', 'utf-8');
 
 const server = http.createServer(getClient);
 
@@ -7,9 +10,11 @@ server.listen(3000);
 console.log('Server start!');
 
 function getClient(req, res) {
-  fs.readFile('./index.html', 'utf-8', (error, data) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
+  const content = ejs.render(index_page, {
+    title: 'Indexページ',
+    content: 'これはテンプレートを使ったサンプルページです',
   });
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write(content);
+  res.end();
 }
