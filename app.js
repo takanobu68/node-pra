b0b93f2ab0990d145fd6e6a10adf31f4ec6b2c0a
Takanobu <61861946+takanobu68@users.noreply.github.com>
2024-06-02 13:46:53 +0900

    ファイルを読み込んで表示

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer(
-    (require,response) =>{
-        response.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>')
-    }
-)
+const server = http.createServer(getClient);
 
-server.listen(3000)
+server.listen(3000);
+console.log('Server start!');
+
+function getClient(req, res) {
+  fs.readFile('./index.html', 'utf-8', (error, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+}
